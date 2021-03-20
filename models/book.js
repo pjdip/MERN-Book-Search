447cc9250055b6f97d832241507217f7f00e9b2d
pjdip <pjdip1@gmail.com>
2021-03-20 18:28:35 -0400

    book model

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const BookSchema = new Schema({
+    title: String,
+    authors: String,
+    description: String,
+    image: String,
+    link: String
+});
+
+const Book = model("Book", BookSchema);
+
+module.exports = Book;
