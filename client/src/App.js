23107ea05176756a5ee0d6aeb618c1f0b3d2ea32
pjdip <pjdip1@gmail.com>
2021-03-22 21:25:50 -0400

    working on footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Container from "../src/components/Container";
 import Nav from "../src/components/Navbar";
 import Saved from "../src/pages/Saved";
 import Search from "../src/pages/Search";
-
+import Footer from "../src/components/Footer";
 
 import logo from './logo.svg';
 import './App.css';
@@ -18,6 +18,7 @@ function App() {
                     <Route exact path="/saved" component={Saved} />
                     <Route exact path="/search" component={Search} />
                 </Container>
+                <Footer />
             </div>
         </Router>
     );
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer align="center">
+            <h2>&copy; Made with ❤️️ by Philip DiPaula</h2>
+        </footer>
+    )
+};
+
+export default Footer;
